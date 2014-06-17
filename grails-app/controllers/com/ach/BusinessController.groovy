0da1a7b4ba7397f1f642708f56f48762d086d27c
Dileep Paudel <creationsdesire@gmail.com>
2014-06-17 17:37:11 +0545

    this file is not actually belong to project

--- grails-app/controllers/com/ach/BusinessController.groovy
+++ grails-app/controllers/com/ach/BusinessController.groovy
@@ -5,15 +5,16 @@ import com.sun.xml.internal.bind.v2.runtime.Name;
 
 class BusinessController {
 	
-	//def BusinessService;
+	//def BusinessService; 
+	//this file is not belong to project. this is just to test if it works or not
 
 	def scaffold=Business
     def index() { }
 	
 	def add(){
-		def category=new Category([name:'Chicken'])
+		def category=new Category([name:'test'])
 		category.save();
-		def business=new Business([address:'Platinum',name:'Cake'])
+		def business=new Business([address:'testing',name:'testing'])
 		business.category=category;
 		business.save();
 		def businesses=Business.list();
